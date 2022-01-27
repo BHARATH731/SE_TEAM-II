73800cb2056fab7849e7a3c2f04d1c633d4b7b84
SrungavarapuPavithra <96069408+SrungavarapuPavithra@users.noreply.github.com>
2022-01-27 21:56:51 +0530

    Update Feedback.java

--- Feedback.java
+++ Feedback.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+/** This class is used to take feedback from the user.
+*@author Team 2
+*@version 1.0
+*@since 2020-11-05
+*/
 public class Feedback{
+	/** This is main method that take feedback from the user and stores it in the feedback file.
+	*@param no args passed
+	*@return it doesn't return anything
+	*@exception throws i/o exception
+	*@see i/o exception.
+	*/
 	public static void run() throws IOException {
 		String fileName = "C:\\Users\\lenovo\\eclipse-workspace\\Feedback.txt";
 		Scanner s=new Scanner(System.in);

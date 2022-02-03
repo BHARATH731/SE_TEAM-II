378f1fb0d10e3997e69db7fc8e2b94400be42e29
SrungavarapuPavithra <96069408+SrungavarapuPavithra@users.noreply.github.com>
2022-02-03 21:32:15 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 import java.io.*;
+/** This class is used to maintain the account.
+*@author Team 2
+*@since 5-11-21
+*@version 1.0
+*/
 public class Account extends Report {
 	 public static void display(String email,String filepath)throws Exception {
 		 Scanner scan = new Scanner( new File(filepath));
@@ -12,8 +17,12 @@ public class Account extends Report {
 	      }
 	      scan.close();
 	}
-	         
-	     
+	 /**This method is used to print the issue and return details of a particular user.
+	 *@return no return type
+	 *@param email
+	 *@exception an exception
+	 *@see Exception
+	 */
 	public static void run(String email)throws Exception {
 		System.out.println("issued books");
 		display(email,filei);
